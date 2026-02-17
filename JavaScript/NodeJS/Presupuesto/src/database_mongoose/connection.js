a7222ac00c476ab2807218887f4b1c07a06f972d
psindra <psindra>
2026-02-17 19:31:30 -0300

    Add MongoDB connection setup with environment variable checks

---
+++ JavaScript/NodeJS/Presupuesto/src/database_mongoose/connection.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+// try {require("dotenv/config")}catch{};
+import "dotenv/config"
+
+while (!process.env["MONGODB_URI"] || !process.env["MONGODB_DATABASE"]) {
+    console.log("Esperando a que se definan las variables de entorno MONGODB_URI y MONGODB_DATABASE...");
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 1 segundo antes de volver a verificar
+}
+
+const MONGODB_URI = process.env["MONGODB_URI"];
+const MONGODB_DATABASE = process.env["MONGODB_DATABASE"];
+console.log({MONGODB_URI, MONGODB_DATABASE});
+
+mongoose.connect( MONGODB_URI + MONGODB_DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(DB=>{
+    console.log("Conectado. Nombre Base de Datos: ", mongoose.connection.name);
+})
+.catch(err=>{
+    console.log(err);
+})
