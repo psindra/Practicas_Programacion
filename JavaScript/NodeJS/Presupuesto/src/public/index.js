e826ad6d2c796188017aecf807d3436131b7a04d
psindra <psindra>
2026-03-08 10:22:41 -0300

    Presupuesto: correción nombre acumuladores anuales (estadísticas)

--- JavaScript/NodeJS/Presupuesto/src/public/index.js
+++ JavaScript/NodeJS/Presupuesto/src/public/index.js
@@ -207,13 +207,13 @@ function renderTabsContent(presupuesto, year) {
                 <p class="mb-2 text-warning-emphasis"><strong>Ahorro/Superávit:</strong> $${presupuesto[year][month]?.estadistica.ahorroMensual?.toLocaleString() || "0"}</span><br>
                 <p class="mb-2 text-muted"><strong>Ahorro Habitual:</strong> $${presupuesto[year][month]?.estadistica.ahorroMensualHabitual?.toLocaleString() || "0"}</span><br>
                 <p class="mb-2 text-muted"><strong>Ahorro Extraordinario:</strong> $${presupuesto[year][month]?.estadistica.ahorroMensualExtraordinario?.toLocaleString() || "0"}</span><br>
-                <p class="mb-2 text-body-tertiary"><strong>Ingresos Acumulados:</strong> $${presupuesto[year][month]?.estadistica.ingresoAcumuladoAnual?.toLocaleString() || "0"}</span><br>
-                <p class="mb-2 text-body-tertiary"><strong>Gastos Acumulados:</strong> $${presupuesto[year][month]?.estadistica.gastoAcumuladoAnual?.toLocaleString() || "0"}</span><br>
-                <p class="mb-2 text-body-tertiary"><strong>Ahorro Acumulado:</strong> $${presupuesto[year][month]?.estadistica.ahorroAcumuladoAnual?.toLocaleString() || "0"}</span><br>
+                <p class="mb-2 text-body-tertiary"><strong>Ingresos Acumulados:</strong> $${presupuesto[year][month]?.estadistica.accumIngreso?.toLocaleString() || "0"}</span><br>
+                <p class="mb-2 text-body-tertiary"><strong>Gastos Acumulados:</strong> $${presupuesto[year][month]?.estadistica.accumGasto?.toLocaleString() || "0"}</span><br>
+                <p class="mb-2 text-body-tertiary"><strong>Ahorro Acumulado:</strong> $${presupuesto[year][month]?.estadistica.accumAhorro?.toLocaleString() || "0"}</span><br>
                 <p class="mb-2 text-success"><strong>Inversiones ARS acumuladas:</strong> $${presupuesto[year][month]?.estadistica.inversionARSAcumuladaAnual?.toLocaleString() || "0"}</span><br>
                 <p class="mb-2 text-success-emphasis"><strong>Inversiones USD acumuladas:</strong> U$D${presupuesto[year][month]?.estadistica.inversionUSDAcumuladaAnual?.toLocaleString() || "0"}</  span><br>
         </div>`;
-        _tabla.caption.innerHTML += `&emsp; - &emsp; Ahorro:${(presupuesto[year][month]?.estadistica.ahorroMensual/_ingresoHabitual * 100).toFixed(1) || "0"} %`;
+        _tabla.caption.innerHTML += `&emsp; - &emsp; Ahorro: ${(presupuesto[year][month]?.estadistica.ahorroMensual/_ingresoHabitual * 100).toFixed(1) || "0"} %`;
         _tabla.caption.innerHTML += `&ensp; - Ahorro Habitual: ${(presupuesto[year][month]?.estadistica.ahorroMensualHabitual/_ingresoHabitual * 100).toFixed(1) || "0"} %`;
         _trEspacio.appendChild(_tdEspacio);
         _tbody.appendChild(_trEspacio);
