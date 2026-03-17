f236bbc7b4012cae5bb6542369347ff12149c979
psindra <psindra>
2026-03-17 19:43:23 -0300

    Presupuesto: nuevo modelo mongoose "Credito"

---
+++ JavaScript/NodeJS/Presupuesto/src/database_mongoose/models/credito.js
@@ -0,0 +1,36 @@
+import mongoose, { Mongoose } from "mongoose";
+import Movimiento from "./movimiento.js";
+
+const esquemaCredito = new mongoose.Schema({
+    monto: {
+        total: {
+            type: Number,
+            validate: [
+                {
+                    validator: (v) => (v == v.toFixed(2)) && v > 0,
+                    message: "El campo Monto.Total debe ser un valor positivo"
+                }
+            ],
+            required: true
+        },
+        nroCuotas: {
+            type: Number,
+            validate: {
+                min: 0,
+                validator: (v) => Number.isInteger(v),
+                message: "El campo Monto.nroCuotas debe ser un valor entero positivo"
+            },
+            required: true,
+        }
+    },
+    formaDebito: {...Movimiento.schema.path("formaPago").options},
+}, { _id: false });
+
+esquemaCredito.pre("validate", function (next) {
+    if (this.formaPago === "Contado" && this.monto.nroCuotas) {
+        throw new Error("La forma de pago 'Contado' no puede tener cuotas. Por favor, elija una forma de pago diferente.");
+    }
+    return;
+});
+export const Credito = Movimiento.discriminator("credito", esquemaCredito);
+export default Credito;
--- JavaScript/NodeJS/Presupuesto/src/database_mongoose/models/modelosPresupuesto.js
+++ JavaScript/NodeJS/Presupuesto/src/database_mongoose/models/modelosPresupuesto.js
@@ -2,5 +2,6 @@ export { Movimiento} from "./movimiento.js";
 export { Ingreso } from "./ingreso.js";
 export { Gasto } from "./gasto.js";
 export { Inversion } from "./inversion.js";
+export { Credito } from "./credito.js";
 
 export { default } from "./movimiento.js";
